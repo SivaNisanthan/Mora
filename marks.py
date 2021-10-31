369151eb4a136da2952aaeff179dc31871723b96
SivaNisanthan <93481761+SivaNisanthan@users.noreply.github.com>
2021-10-31 08:48:38 -0700

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,16 @@
+n=int(input('enter the number of sudents')
+if 2<=n<=10
+      name=[]
+      marks=[]
+      for i in range(1,n+1):
+          t=input('enter the name and marks')
+          t.split()
+          na,ma=t[0],t[1:]
+          name.append(na)
+          marks.append(ma)
+      print(marks)
+      print(name)
+      
+      
+      
+      
